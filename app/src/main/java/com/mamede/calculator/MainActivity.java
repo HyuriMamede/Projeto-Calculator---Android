fab77113c03f8397cbd7c9acc149418e5f069ef0
Hyuri Mamede <hyurimamede@outlook.com>
2022-11-19 15:13:36 -0300

    Finalizado estica dos botões

--- app/src/main/java/com/mamede/calculator/MainActivity.java
+++ app/src/main/java/com/mamede/calculator/MainActivity.java
@@ -3,12 +3,22 @@ package com.mamede.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button numeroZero, numeroUm, numeroDois, numeroTres, numeroQuatro, numeroCinco, numeroSeis,
+    numeroSete, numeroOito, numeroNove, virgula, subtracao,soma, multiplicao, divisao, igual, limpar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+
     }
+
 }
